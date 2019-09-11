a1279a69bfbd535cf2263e1a9af49d39da3e4919
mbarrins <maija.barrins@gmail.com>
2019-09-11 13:13:54 +0100

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/itemAPI');
@@ -8,6 +9,12 @@ const port = process.env.PORT || 3000;
 const Item = require('./models/itemModel');
 const itemRouter = require('./routes/itemRouter')(Item);
 
+const corsOptions = {
+  origin: 'http://localhost:3001',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
